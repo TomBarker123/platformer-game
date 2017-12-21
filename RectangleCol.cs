6541a77e9b39fd54438431223b0785e7aa124c4a
Tom Barker <32296867+TomBarker123@users.noreply.github.com>
2017-12-21 16:46:02 +0000

    Update RectangleCol.cs

--- RectangleCol.cs
+++ RectangleCol.cs
@@ -11,7 +11,7 @@ namespace GameNew
 {
     static class RectangleCol
     {
-        public static bool TouchTopOF(this Rectangle r1, Rectangle r2)
+        public static bool TouchTopOf(this Rectangle r1, Rectangle r2)
         {
             return (r1.Bottom >= r2.Top - 1 && r1.Bottom <= r2.Top + (r2.Height / 2) && r1.Right >= r2.Left + (r2.Width / 5) && r1.Left <= r2.Right - (r2.Width / 5));
         }
